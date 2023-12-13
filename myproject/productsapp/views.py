72c9c5fc1294e287cd724ffe814fccf39305ff3a
Poorvaja Satha Sivam <poorvaja.sathasivam@outlook.com>
2023-12-13 14:44:48 +0530

    Implementing views

--- myproject/productsapp/views.py
+++ myproject/productsapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+# myapp/views.py
+
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
