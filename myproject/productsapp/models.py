6a39c69b59d62e38217b6f6131bd07082d3f8b8a
Poorvaja Satha Sivam <poorvaja.sathasivam@outlook.com>
2023-12-13 14:42:31 +0530

    Implementing models

--- myproject/productsapp/models.py
+++ myproject/productsapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
